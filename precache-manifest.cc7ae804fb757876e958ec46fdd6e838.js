a1544e6424dd15bfd327a025b8cd80563c45e7e7
gulbazsharif <71559937+gulbazsharif@users.noreply.github.com>
2020-09-23 00:07:08 +0500

    Updates

---
+++ precache-manifest.cc7ae804fb757876e958ec46fdd6e838.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "879c98a1e3ac2df621752e51550cfff0",
+    "url": "/hello/index.html"
+  },
+  {
+    "revision": "9213470a42034651763b",
+    "url": "/hello/static/css/main.cff1c470.chunk.css"
+  },
+  {
+    "revision": "8eded8f7234818dea64e",
+    "url": "/hello/static/js/2.3c745b8e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hello/static/js/2.3c745b8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9213470a42034651763b",
+    "url": "/hello/static/js/main.e58b684a.chunk.js"
+  },
+  {
+    "revision": "5c2c3633944d7cf612de",
+    "url": "/hello/static/js/runtime-main.bab1fdc9.js"
+  },
+  {
+    "revision": "1bd5606a2ba79bc2f9366ad272003a35",
+    "url": "/hello/static/media/img.1bd5606a.jpg"
+  }
+]);
